27e073d6caba001ec16b650816ca8649598092a2
Mint <143609582+mintrgm@users.noreply.github.com>
2025-03-16 22:11:21 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
+import Login from "./components/Login";
+import Signup from "./components/Signup"; 
 
 function App() {
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
   );
 }
 
