3b64f37f6cf352f5ce15cdcb1264ccad999b95d4
Mint <143609582+mintrgm@users.noreply.github.com>
2025-04-11 00:22:44 +0545

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,30 +1,54 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import { useAuth } from "../App";
 import "./Navbar.css";
-import cartIcon from "../assets/cart.png"; 
-import defaultAvatar from "../assets/avatar.jpeg"; 
+import cartIcon from "../assets/cart.png";
+import defaultAvatar from "../assets/avatar.jpeg";
 
-const Navbar = () => {
+const Navbar = ({ setShowCart }) => {
   const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const handleCartClick = () => {
+    if (!user) {
+      navigate("/login");
+    } else {
+      setShowCart(true);
+    }
+  };
 
   return (
     <nav className="navbar">
       <h1 className="logo">KuruKuru Petto Shoppu</h1>
       <ul className="nav-links">
         <li>
-          <Link to="/">HOME</Link>
+          <Link
+            to="/"
+            className={location.pathname === "/" ? "nav-active" : "nav-inactive"}
+          >
+            HOME
+          </Link>
         </li>
         <li>
-          <Link to="/collections">COLLECTIONS</Link>
+          <Link
+            to="/collections"
+            className={location.pathname === "/collections" ? "nav-active" : "nav-inactive"}
+          >
+            COLLECTIONS
+          </Link>
         </li>
       </ul>
       <div className="nav-right">
         {user ? (
           <>
-            <Link to="/cart">
-              <img src={cartIcon} alt="Cart" className="cart-icon" />
-            </Link>
+            <img
+              src={cartIcon}
+              alt="Cart"
+              className="cart-icon"
+              onClick={handleCartClick}
+              style={{ cursor: "pointer" }}
+            />
             <Link to="/profile">
               <img
                 src={user.profilePicture || defaultAvatar}
